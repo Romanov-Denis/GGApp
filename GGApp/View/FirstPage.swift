4d818e6aefd23b27c962fdeb879f5455f664c61d
Romanov-Denis <110339205+Romanov-Denis@users.noreply.github.com>
2023-08-28 10:06:29 +0700

    first commit

--- GGApp/View/FirstPage.swift
+++ GGApp/View/FirstPage.swift
@@ -9,58 +9,52 @@ import SwiftUI
 
 struct FirstPage: View {
     var body: some View {
-        ZStack {
-            Image("inside")
-            Color(.black)
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                VStack (spacing: 60) {
-                    Button() {
-                        print("Переходим к услуге")
-                    } label: {
-                        Text("Услуга")
-                            .foregroundColor(.orange)
-                            .font(.title)
-                    }
-                    .frame(width: 280,height: 60)
-                    .overlay(Capsule().stroke(Color.orange,lineWidth: 5))
-                    
-                    
-                    
-                    
-                    Button() {
-                        print("Переходим к сотрудникам")
-                    } label: {
-                        Text("Сотрудники")
-                            .foregroundColor(.orange)
-                            .font(.title)
-                    }
-                    .frame(width: 280,height: 60)
-                    .overlay(Capsule().stroke(Color.orange,lineWidth: 5))
-                    
-                    
-                    Button() {
-                        print("Переходим к дате и времени")
-                    } label: {
-                        Text("Дата и время")
-                            .foregroundColor(.orange)
-                            .font(.title)
-                    }
-                    .frame(width: 280,height: 60)
-                    .overlay(Capsule().stroke(Color.orange,lineWidth: 5))
+        VStack{
+            VStack (spacing: 60) {
+                Button() {
+                    print("Переходим к услуге")
+                } label: {
+                    Text("Услуга")
+                        .foregroundColor(.orange)
+                        .font(.title)
                 }
+                .frame(width: 280,height: 60)
+                .overlay(Capsule().stroke(Color.orange,lineWidth: 5))
+                
+                Button() {
+                    print("Переходим к услуге")
+                } label: {
+                    Text("Сотрудник")
+                        .foregroundColor(.orange)
+                        .font(.title)
+                }
+                .frame(width: 280,height: 60)
+                .overlay(Capsule().stroke(Color.orange,lineWidth: 5))
+                
+                Button() {
+                    print("Переходим к услуге")
+                } label: {
+                    Text("Дата и время")
+                        .foregroundColor(.orange)
+                        .font(.title)
+                }
+                .frame(width: 280,height: 60)
+                .overlay(Capsule().stroke(Color.orange,lineWidth: 5))
                 
             }
+            .background(Image("back"))
+                .blur(radius: 10)
+        }
+       
+    }
+}
+                    
+        
+        
+
+    
+    struct FirstPage_Previews: PreviewProvider {
+        static var previews: some View {
+            FirstPage()
         }
     }
-}
-
-
-
-
-
-struct FirstPage_Previews: PreviewProvider {
-    static var previews: some View {
-        FirstPage()
-    }
-}
--- GGApp/View/RegisterPage.swift
+++ GGApp/View/RegisterPage.swift
@@ -8,8 +8,102 @@
 import SwiftUI
 
 struct RegisterPage: View {
+    
+    @State private var isAuth = true
+    
+    
+    @State private var phoneNumber = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    
+    @State private var isTabViewShow = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack() {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 300, height: 150, alignment: .topLeading)
+                    .padding(.vertical, 0.0)
+                VStack () {
+                    Text(isAuth ? "Авторизация" : "Регистрация")
+                        .padding()
+                        .font(.title2.bold())
+                        .background(Color("half-white"))
+                        .cornerRadius(15)
+                    VStack {
+                        TextField("Номер телефона", text: $phoneNumber)
+                            .padding()
+                            .background(Color("half-white"))
+                            .cornerRadius(15)
+                            .padding(8)
+                            .padding(.horizontal, 12)
+                        
+                        SecureField("Пароль", text: $password)
+                            .padding()
+                            .background(Color("half-white"))
+                            .cornerRadius(15)
+                            .padding(8)
+                            .padding(.horizontal, 12)
+                        
+                        if !isAuth {
+                            SecureField("Повторите пароль", text: $confirmPassword)
+                                .padding()
+                                .background(Color("half-white"))
+                                .cornerRadius(15)
+                                .padding(8)
+                                .padding(.horizontal, 12)
+                        }
+                        
+                        Button {
+                            if isAuth {
+                                print("Авторизация")
+                                isTabViewShow.toggle()
+                            } else {
+                                self.phoneNumber = ""
+                                self.password = ""
+                                self.confirmPassword = ""
+                                self.isAuth.toggle()
+                            }
+                        } label: {
+                            Text(isAuth ? "Войти" : "Создать профиль")
+                        }
+                        .foregroundColor(.black)
+                        .font(.title3.bold())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color("half-white"))
+                        .cornerRadius(15)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        
+                        Button {
+                            isAuth.toggle()
+                        } label: {
+                            Text(isAuth ? "Впервые у нас?" : "Уже есть аккаунт")
+                        }
+                        .foregroundColor(.black)
+                        .font(.title3.bold())
+                        .padding(.horizontal )
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(15)
+                        .padding(8)
+                        .padding(.horizontal, 12)
+                        
+                        
+                    }
+                    .padding()
+                    .background(Color("half-white"))
+                    .cornerRadius(15)
+                    .padding()
+                }
+                
+            }
+            .frame (maxWidth: .infinity, maxHeight: .infinity)
+            .background(Image("back").blur(radius: 10))
+            .animation(Animation.easeOut(duration: 0.3), value: isAuth)
+            .fullScreenCover(isPresented: $isTabViewShow) {
+                FirstPage()
+            }
     }
 }
 
